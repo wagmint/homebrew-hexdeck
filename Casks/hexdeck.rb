b8a8cc4a495190e5e95271af14457d167feb896d
nousnamez <jakejin12.finance@gmail.com>
2026-02-25 19:38:02 -0500

    fix(hexdeck): update aarch64 DMG sha256 for v0.4.2 (re-signed clean bundle)

--- Casks/hexdeck.rb
+++ Casks/hexdeck.rb
@@ -3,7 +3,7 @@ cask "hexdeck" do
 
   on_arm do
     url "https://github.com/wagmint/hexdeck/releases/download/menubar-v0.4.2/Hexdeck_0.4.2_aarch64.dmg"
-    sha256 "441698ef8f30640085fd837e9eeb02edd06797996f234f26fc71c46470a692bb"
+    sha256 "4b1f9f92a9e983019d89fe8230c8d0616e7d7874649e73c9583d1361973a1c74"
   end
 
   on_intel do
